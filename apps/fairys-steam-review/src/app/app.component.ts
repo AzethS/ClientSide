736c726c5490ca2101b2ae5c5a7cf38ce413d125
Fairys Ardhana <fairys.ardhana@gmail.com>
2024-04-09 16:59:07 +0200

    frontend calling api

--- apps/fairys-steam-review/src/app/app.component.ts
+++ apps/fairys-steam-review/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+// import { FeaturesModule } from '@avans-nx-workshop/share-a-meal/features';
+import { FeaturesModule } from '../../../../libs/fairys-steam-review/features/src/lib/features.module';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, FeaturesModule],
   selector: 'avans-nx-workshop-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
