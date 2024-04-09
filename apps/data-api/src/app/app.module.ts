1d7a0c12ed341d0849e9bf6eb61a7f26c973be4a
Fairys Ardhana <fairys.ardhana@gmail.com>
2024-04-09 13:22:32 +0200

    api en nest backend

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BackendFeaturesReviewModule } from "../../../../libs/backend/features/src/lib/backend-features-review.module";
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [BackendFeaturesReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
