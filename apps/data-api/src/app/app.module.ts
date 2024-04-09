d86bd113a6c552b32b883f0275c39f4cc10d2611
Fairys Ardhana <fairys.ardhana@gmail.com>
2024-04-09 19:23:14 +0200

    Shared library

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BackendFeaturesReviewModule } from '@avans-nx-workshop/backend/features';
-
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [BackendFeaturesReviewModule],
+  imports: [BackendFeaturesReviewModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
